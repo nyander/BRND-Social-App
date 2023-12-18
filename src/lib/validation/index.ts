3840751c3a36228e40beed81c6750ffcaf9c82bd
Richard Nyande <rich.nyande@gmail.com>
2023-12-18 15:36:02 +0000

    what is wrong

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,27 @@
+import * as z from "zod"
+
+export const SignUpValidation = z.object({
+    name: z.string().min(2,{
+        message: "Name must be at least 2 characters",
+    }),
+    username: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }).max(50, {
+      message: "Username must be at max 50 characters.",
+    }),
+    email:z.string().email(),
+    password: z.string().min(8, {message: "Password must be at least 8 characters."}),
+  })
+
+
+  export const SignInValidation = z.object({
+    email:z.string().email(),
+    password: z.string().min(8, {message: "Password must be at least 8 characters."}),
+  })
+
+  export const PostValidation = z.object({
+   caption: z.string().min(5).max(2200),
+   file: z.custom<File[]>(),
+   location: z.string().min(2).max(100),
+   tags: z.string(),
+  })
